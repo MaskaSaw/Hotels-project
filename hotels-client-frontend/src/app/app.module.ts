46b7010be82abcc7c58f2565a308736c34ce003d
Vladislav Marchyk <37183107+MaskaSaw@users.noreply.github.com>
2021-04-07 16:40:48 +0300

    Datepicker refactoring

--- hotels-client-frontend/src/app/app.module.ts
+++ hotels-client-frontend/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from  '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -48,6 +50,8 @@ import { PasswordValidatorDirective} from './validators/password.validator';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     BsDatepickerModule.forRoot(), 
     DatepickerModule.forRoot(),
     TypeaheadModule.forRoot()
--- hotels-client-frontend/src/app/params.ts
+++ hotels-client-frontend/src/app/params.ts
@@ -9,6 +9,7 @@ export class Params {
   constructor() {
     this.checkIn = new Date();
     this.checkOut = new Date();
+    this.checkOut.setDate(this.checkIn.getDate() + 1);
     this.hotelName = '';
     this.country = '';
     this.city = '';
